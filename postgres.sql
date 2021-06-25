7e3e843534ef0bf6ff623403a3523e888c06838b
Francis Yao <neko.makie27@gmail.com>
2021-06-24 21:55:52 -0700

    updated postgres.sql create table commands

--- postgres.sql
+++ postgres.sql
@@ -2,64 +2,60 @@ CREATE DATABASE reviewsDB;
 USE reviewsDB;
 
 CREATE TABLE reviews (
-  id INT NOT NULL PRIMARY KEY,
-  product_id INT UNIQUE NOT NULL,
+  id INT,
+  product_id INT NOT NULL,
   rating INT,
-  date_created INT,
-  summary VARCHAR (60),
+  date_created BIGINT,
+  summary TEXT,
   body TEXT,
-  recommend BOOLEAN.
+  recommend BOOLEAN,
   reported BOOLEAN,
   reviewer_name TEXT,
   reviewer_email TEXT,
   response TEXT,
-  helpfulness INT
-)
+  helpfulness INT,
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE review_photos (
-  id INT NOT NULL PRIMARY KEY,
-  review_id NOT NULL,
+  id INT,
+  review_id INT,
   photo_url TEXT,
-  FOREIGN KEY (review_id)
-    REFERENCES reviews (id)
-)
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE characteristics (
-  id INT NOT NULL PRIMARY KEY,
+  id INT,
   product_id INT,
   characteristic_name VARCHAR,
-  FOREIGN KEY (product_id)
-    REFERENCES reviews(product_id)
-)
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE characteristics_reviews (
-  id INT NOT NULL PRIMARY KEY,
+  id INT,
   characteristic_id INT,
   review_id INT,
   characteristic_value INT,
-  FOREIGN KEY (review_id)
-    REFERENCES reviews (id)
-  FOREIGN KEY (characteristic_id)
-    REFERENCES characteristics (id)
-)
+  PRIMARY KEY (id)
+);
 
 
 COPY reviews(id, product_id, rating, date_created, summary, body,recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
-FROM '\csv\reviews.csv'
+FROM '/home/franciskyao/Downloads/reviews.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY review_photos(id, review_id, photo_url)
-FROM '\csv\review_photos.csv'
+FROM '/home/franciskyao/Downloads/reviews_photos.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY characteristics(id, product_id, characteristic_name)
-FROM '\csv\characteristics.csv'
+FROM '/home/franciskyao/Downloads/characteristics.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY characteristics_reviews(id, characteristic_id, review_id, characteristic_value)
-FROM '\csv\characteristics_reviews'
+FROM '/home/franciskyao/Downloads/characteristic_reviews.csv'
 DELIMITER ','
 CSV HEADER;
